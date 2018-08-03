913da5691024ca7ffb11b86cbdf2a3b54f234edc
angadpc <angad.chandratre@gmail.com>
2018-08-03 22:54:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // add solution here
+function theBeatlesPlay(musicArray, instrumentArray) {
+  var array = [];
+  for (var i = 0; i < musicArray.length; i++) {
+    var newStr = musicArray[i] + " plays " + instrumentArray[i];
+    array[i] = newStr;
+  }
+  return array;
+}
+
+function johnLennonFacts(factsArray) {
+
+}
